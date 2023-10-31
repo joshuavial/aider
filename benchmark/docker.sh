8f639cccbba1e40c98866b45584bdef300ce67ac
Joshua Vial <joshua@enspiral.com>
2023-11-01 11:10:52 +1300

    config for openrouter

--- benchmark/docker.sh
+++ benchmark/docker.sh
@@ -4,7 +4,8 @@ docker run \
        -it --rm \
        -v `pwd`:/aider \
        -v `pwd`/tmp.benchmarks/.:/benchmarks \
-       -e OPENAI_API_KEY=$OPENAI_API_KEY \
+       -e OPENAI_API_KEY=$OPENROUTER_API_KEY \
+       -e OPENAI_API_BASE='https://openrouter.ai/api/v1' \
        -e HISTFILE=/aider/.bash_history \
        -e AIDER_DOCKER=1 \
        -e AIDER_BENCHMARK_DIR=/benchmarks \
